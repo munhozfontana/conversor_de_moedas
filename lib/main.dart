3bb201d531e812da759b33915935d23b43b9df52
luís fernando <munhozfontana@gmail.com>
2019-11-27 07:04:03 -0300

    finalização da inferface

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,13 @@ void main() async {
   runApp(
     MaterialApp(
       home: Home(),
+      theme: ThemeData(
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+          ),
+        ),
+      ),
     ),
   );
 }
@@ -20,6 +27,9 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  double dolar;
+  double euro;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,7 +41,7 @@ class _HomeState extends State<Home> {
       ),
       body: FutureBuilder(
         future: getData(),
-        builder: (convext, snapshot) {
+        builder: (context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.none:
             case ConnectionState.waiting:
@@ -58,10 +68,62 @@ class _HomeState extends State<Home> {
                   ),
                 );
               } else {
-                return Container(
-                  color: Colors.green,
+                print(snapshot.data);
+                dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+                return SingleChildScrollView(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Icon(
+                        Icons.monetization_on,
+                        size: 150,
+                        color: Colors.amber,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: "Reais",
+                          labelStyle: TextStyle(color: Colors.amber),
+                          border: OutlineInputBorder(),
+                          prefixText: "RS\$",
+                        ),
+                        style: TextStyle(
+                          color: Colors.amber,
+                          fontSize: 25,
+                        ),
+                      ),
+                      Divider(),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: "Dolar",
+                          labelStyle: TextStyle(color: Colors.amber),
+                          border: OutlineInputBorder(),
+                          prefixText: "US\$",
+                        ),
+                        style: TextStyle(
+                          color: Colors.amber,
+                          fontSize: 25,
+                        ),
+                      ),
+                      Divider(),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: "Euros",
+                          labelStyle: TextStyle(color: Colors.amber),
+                          border: OutlineInputBorder(),
+                          prefixText: "€",
+                        ),
+                        style: TextStyle(
+                          color: Colors.amber,
+                          fontSize: 25,
+                        ),
+                      ),
+                    ],
+                  ),
                 );
               }
+              return null;
           }
         },
       ),
@@ -71,5 +133,9 @@ class _HomeState extends State<Home> {
 
 Future<Map> getData() async {
   http.Response response = await http.get(uri);
-  print(jsonDecode(response.body)['results']['currencies']['USD']);
+  if (response.statusCode == 200) {
+    return jsonDecode(response.body);
+  } else {
+    throw Exception('Falhou ao carregar os dados');
+  }
 }
