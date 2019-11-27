510d645fbfd5b2b9b6de4da7d5ae5d446a388357
luís fernando <munhozfontana@gmail.com>
2019-11-27 12:01:15 -0300

    implementando future build

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,69 @@ import 'package:http/http.dart' as http;
 const uri = "https://api.hgbrasil.com/finance";
 
 void main() async {
-  http.Response response = await http.get(uri);
-  print(jsonDecode(response.body)['results']['currencies']['USD']);
-
   runApp(
     MaterialApp(
-      home: Container(),
+      home: Home(),
     ),
   );
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("\$ Conversor \$"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+      body: FutureBuilder(
+        future: getData(),
+        builder: (convext, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return Center(
+                child: Text(
+                  "Carregando dados",
+                  style: TextStyle(
+                    color: Colors.amber,
+                    fontSize: 25,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              );
+            default:
+              if (snapshot.hasError) {
+                Center(
+                  child: Text(
+                    "Carregando dados",
+                    style: TextStyle(
+                      color: Colors.amber,
+                      fontSize: 25,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              } else {
+                return Container(
+                  color: Colors.green,
+                );
+              }
+          }
+        },
+      ),
+    );
+  }
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(uri);
+  print(jsonDecode(response.body)['results']['currencies']['USD']);
 }
